ab40e8aeb6696446ed633fa0b5f1f38e217f3dda
Qianyun Mu <msbluemu@gmail.com>
2023-05-16 12:26:57 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Qianyun Mu",
+    streetAddress: "24-36 Pacific Highway",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+   const newObj = {...employee};
+   newObj[key] = value;
+   return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObj = {...employee};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
